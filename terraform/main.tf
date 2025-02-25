bcbe0970dcb462b9b7506796d635e6a8677610ac
theankitamahajan <ankita.mahajan377@gmail.com>
2025-02-25 15:49:55 -0500

    Remove Azure Bastion and optimize Terraform code for cost efficiency

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,84 +1,113 @@
-# Configure the Azure provider
 provider "azurerm" {
   features {}
+  subscription_id = "dfaf27e3-3448-4dbd-a4a8-2016aed9f693" # Replace with your Azure subscription ID
 }
 
-# Create a resource group
+# Resource Group
 resource "azurerm_resource_group" "maveric" {
-  name     = "Maveric"
-  location = "East US"
+  name     = "maveric"
+  location = "East US" # Choose a cost-friendly region
 }
 
-# Create an Azure Container Registry (ACR)
-resource "azurerm_container_registry" "acr" {
-  name                = "mavericacr123" # Must be globally unique
-  resource_group_name = azurerm_resource_group.maveric.name
-  location            = azurerm_resource_group.maveric.location
-  sku                 = "Basic"        # Options: Basic, Standard, Premium
-  admin_enabled       = true           # Enable admin access
-}
-
-# Create a Virtual Network
-resource "azurerm_virtual_network" "vnet" {
+# Virtual Network
+resource "azurerm_virtual_network" "maveric_vnet" {
   name                = "maveric-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.maveric.location
   resource_group_name = azurerm_resource_group.maveric.name
 }
 
-# Create a Public Subnet
-resource "azurerm_subnet" "public_subnet" {
-  name                 = "public-subnet"
+# Subnet
+resource "azurerm_subnet" "maveric_subnet" {
+  name                 = "maveric-subnet"
   resource_group_name  = azurerm_resource_group.maveric.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  virtual_network_name = azurerm_virtual_network.maveric_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Create a Public IP for the VM
-resource "azurerm_public_ip" "vm_public_ip" {
-  name                = "vm-public-ip"
+# Public IP (Standard SKU with Static Allocation)
+resource "azurerm_public_ip" "maveric_public_ip" {
+  name                = "maveric-public-ip"
   location            = azurerm_resource_group.maveric.location
   resource_group_name = azurerm_resource_group.maveric.name
   allocation_method   = "Static" # Required for Standard SKU
-  sku                 = "Standard" # Set SKU to Standard
+  sku                 = "Standard" # Use Standard SKU
 }
 
-# Create a Network Interface for the VM
-resource "azurerm_network_interface" "vm_nic" {
-  name                = "vm-nic"
+# Network Security Group (NSG) to allow SSH traffic
+resource "azurerm_network_security_group" "maveric_nsg" {
+  name                = "maveric-nsg"
+  location            = azurerm_resource_group.maveric.location
+  resource_group_name = azurerm_resource_group.maveric.name
+
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*" # Allow all IPs (or specify your IP)
+    destination_address_prefix = "*"
+  }
+}
+
+# Associate NSG with the subnet
+resource "azurerm_subnet_network_security_group_association" "maveric_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.maveric_subnet.id
+  network_security_group_id = azurerm_network_security_group.maveric_nsg.id
+}
+
+# Network Interface
+resource "azurerm_network_interface" "maveric_nic" {
+  name                = "maveric-nic"
   location            = azurerm_resource_group.maveric.location
   resource_group_name = azurerm_resource_group.maveric.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.public_subnet.id
+    subnet_id                     = azurerm_subnet.maveric_subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
+    public_ip_address_id          = azurerm_public_ip.maveric_public_ip.id # Associate Public IP
   }
 }
 
-# Create an Ubuntu VM
-resource "azurerm_linux_virtual_machine" "ubuntu_vm" {
-  name                = "ubuntu-vm"
+# Linux Virtual Machine
+resource "azurerm_linux_virtual_machine" "maveric_vm" {
+  name                = "maveric-vm"
   resource_group_name = azurerm_resource_group.maveric.name
   location            = azurerm_resource_group.maveric.location
-  size                = "Standard_B1s" # VM size
-  admin_username      = "adminuser"
-  admin_password      = "Ankita12@" # Set the admin password
-  disable_password_authentication = false # Enable password authentication
+  size                = "Standard_B1s" # Cost-friendly VM size
+  admin_username      = "azureuser"
   network_interface_ids = [
-    azurerm_network_interface.vm_nic.id,
+    azurerm_network_interface.maveric_nic.id, # Use the correct NIC
   ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("${path.module}/week1-dev.pub") # Use the public key
+  }
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    storage_account_type = "Standard_LRS" # Cost-friendly storage
   }
 
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS" # Ubuntu version
+    sku       = "18.04-LTS" # Free tier eligible
     version   = "latest"
   }
 }
+
+# Azure Container Registry (ACR)
+resource "azurerm_container_registry" "maveric_acr" {
+  name                = "mavericacr"
+  resource_group_name = azurerm_resource_group.maveric.name
+  location            = azurerm_resource_group.maveric.location
+  sku                 = "Basic" # Cost-friendly ACR SKU
+  admin_enabled       = true
+}
+
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,46 +1,8 @@
-# Resource Group Output
-output "resource_group_name" {
-  description = "Name of the resource group"
-  value       = azurerm_resource_group.maveric.name
+# Outputs
+output "vm_public_ip" {
+  value = azurerm_public_ip.maveric_public_ip.ip_address
 }
 
-# Virtual Network Output
-output "virtual_network_name" {
-  description = "Name of the virtual network"
-  value       = azurerm_virtual_network.vnet.name
-}
-
-# Public Subnet Output
-output "public_subnet_name" {
-  description = "Name of the public subnet"
-  value       = azurerm_subnet.public_subnet.name
-}
-
-# Public IP Output
-output "public_ip_address" {
-  description = "Public IP address of the VM"
-  value       = azurerm_public_ip.vm_public_ip.ip_address
-}
-
-# Network Interface Output
-output "network_interface_name" {
-  description = "Name of the network interface"
-  value       = azurerm_network_interface.vm_nic.name
-}
-
-# Virtual Machine Output
-output "vm_name" {
-  description = "Name of the virtual machine"
-  value       = azurerm_linux_virtual_machine.ubuntu_vm.name
-}
-
-output "vm_admin_username" {
-  description = "Admin username for the virtual machine"
-  value       = azurerm_linux_virtual_machine.ubuntu_vm.admin_username
-}
-
-# ACR Repository Name Output
-output "acr_repository_name" {
-  description = "Name of the Azure Container Registry (ACR)"
-  value       = azurerm_container_registry.acr.name
+output "acr_login_server" {
+  value = azurerm_container_registry.maveric_acr.login_server
 }
