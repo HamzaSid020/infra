ed6fd8f0e9955a71334f175661457f2f5a3c68cf
theankitamahajan <ankita.mahajan377@gmail.com>
2025-03-29 09:48:23 -0400

    Changes to variables

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -2,3 +2,18 @@ variable "location" {
   description = "The Azure region to deploy resources"
   default     = "East US"
 }
+
+variable "jwt_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "resend_api" {
+  type      = string
+  sensitive = true
+}
+
+variable "firebase_creds" {
+  type      = string
+  sensitive = true
+}
