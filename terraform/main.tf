48c7795b5d68918bcf03ca57827fce91faf9ae7a
theankitamahajan <ankita.mahajan377@gmail.com>
2025-04-10 01:17:06 -0400

    Adding K8s manifests & updating terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,201 +1,59 @@
-# main.tf
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
 
-# Provider configuration
 provider "azurerm" {
   features {}
-  subscription_id = "0cb69517-b7d3-4bde-a4b7-3905c5b3d55c" # Your Azure subscription ID
-}
-
-# Variables for SSH key paths
-variable "private_key_path" {
-  description = "Path to the SSH private key"
-  default     = "~/.ssh/terraform/terraform_key"
-}
-
-variable "public_key_path" {
-  description = "Path to the SSH public key"
-  default     = "~/.ssh/terraform/terraform_key.pub"
 }
 
 # Resource Group
-resource "azurerm_resource_group" "maveric" {
-  name     = "Maverics"
-  location = "East US" # Choose a cost-friendly region
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-# Virtual Network
-resource "azurerm_virtual_network" "maveric_vnet" {
-  name                = "maveric-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.maveric.location
-  resource_group_name = azurerm_resource_group.maveric.name
+# Azure Container Registry (ACR)
+resource "azurerm_container_registry" "acr" {
+  name                     = var.acr_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  sku                      = "Basic"
+  admin_enabled            = true
 }
 
-# Subnet
-resource "azurerm_subnet" "maveric_subnet" {
-  name                 = "maveric-subnet"
-  resource_group_name  = azurerm_resource_group.maveric.name
-  virtual_network_name = azurerm_virtual_network.maveric_vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+# AKS Cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.dns_prefix
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = var.vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin     = "azure"
+    load_balancer_sku  = "standard"
+  }
+
+  depends_on = [azurerm_container_registry.acr]
 }
 
-# Public IP (Standard SKU with Static Allocation)
-resource "azurerm_public_ip" "maveric_public_ip" {
-  name                = "maveric-public-ip"
-  location            = azurerm_resource_group.maveric.location
-  resource_group_name = azurerm_resource_group.maveric.name
-  allocation_method   = "Static"   # Required for Standard SKU
-  sku                 = "Standard" # Use Standard SKU
-}
-
-# Network Security Group (NSG) with additional ports
-resource "azurerm_network_security_group" "maveric_nsg" {
-  name                = "maveric-nsg"
-  location            = azurerm_resource_group.maveric.location
-  resource_group_name = azurerm_resource_group.maveric.name
-
-  # Rule for SSH (port 22)
-  security_rule {
-    name                       = "allow-ssh"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*" # Allow all IPs (or specify your IP)
-    destination_address_prefix = "*"
-  }
-
-  # Rule for port 3000 (Frontend)
-  security_rule {
-    name                       = "allow-port-3000"
-    priority                   = 110
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "3000"
-    source_address_prefix      = "*" # Allow all IPs (or specify your IP)
-    destination_address_prefix = "*"
-  }
-
-  # Rule for port 4004 (Caretaker Service)
-  security_rule {
-    name                       = "allow-port-4004"
-    priority                   = 120
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "4004"
-    source_address_prefix      = "*" # Allow all IPs (or specify your IP)
-    destination_address_prefix = "*"
-  }
-
-  # Rule for port 4000 (Auth Service)
-  security_rule {
-    name                       = "allow-port-4000"
-    priority                   = 130
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "4000"
-    source_address_prefix      = "*" # Allow all IPs (or specify your IP)
-    destination_address_prefix = "*"
-  }
-
-  # Rule for port 4005 (Reminder Service)
-  security_rule {
-    name                       = "allow-port-4005"
-    priority                   = 125
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "4005"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  # Rule for port 3001 (Middleware)
-  security_rule {
-    name                       = "allow-port-3001"
-    priority                   = 140
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "3001"
-    source_address_prefix      = "*" # Allow all IPs (or specify your IP)
-    destination_address_prefix = "*"
-  }
-
-  # Rule for port 4002 (Medication Service)
-  security_rule {
-    name                       = "allow-port-4002"
-    priority                   = 150
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "4002"
-    source_address_prefix      = "*" # Allow all IPs (or specify your IP)
-    destination_address_prefix = "*"
-  }
-}
-
-# Associate NSG with the subnet
-resource "azurerm_subnet_network_security_group_association" "maveric_nsg_assoc" {
-  subnet_id                 = azurerm_subnet.maveric_subnet.id
-  network_security_group_id = azurerm_network_security_group.maveric_nsg.id
-}
-
-# Network Interface
-resource "azurerm_network_interface" "maveric_nic" {
-  name                = "maveric-nic"
-  location            = azurerm_resource_group.maveric.location
-  resource_group_name = azurerm_resource_group.maveric.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.maveric_subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.maveric_public_ip.id # Associate Public IP
-  }
-}
-
-# Linux Virtual Machine (Ubuntu)
-resource "azurerm_linux_virtual_machine" "maveric_vm" {
-  name                = "maveric-vm"
-  resource_group_name = azurerm_resource_group.maveric.name
-  location            = azurerm_resource_group.maveric.location
-  size                = "Standard_B1s" # Cost-friendly VM size
-  admin_username      = "azureuser"
-  network_interface_ids = [
-    azurerm_network_interface.maveric_nic.id,
-  ]
-
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = file(var.public_key_path)
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-  custom_data = base64encode(templatefile("${path.module}/install.tpl.sh", {
-    jwt_secret     = var.jwt_secret,
-    resend_api     = var.resend_api,
-    firebase_creds = var.firebase_creds
-  }))
+# ACR & AKS Integration
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
 }
--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,19 +1,34 @@
+variable "resource_group_name" {
+  default     = "microservice-rg"
+  description = "Name of the resource group"
+}
+
 variable "location" {
-  description = "The Azure region to deploy resources"
   default     = "East US"
+  description = "Azure region for deployment"
 }
 
-variable "jwt_secret" {
-  type      = string
-  sensitive = true
+variable "acr_name" {
+  default     = "microserviceacr123"
+  description = "Unique name for Azure Container Registry"
 }
 
-variable "resend_api" {
-  type      = string
-  sensitive = true
+variable "aks_cluster_name" {
+  default     = "microservice-aks"
+  description = "Name of your AKS cluster"
 }
 
-variable "firebase_creds" {
-  type      = string
-  sensitive = true
+variable "dns_prefix" {
+  default     = "microserviceaks"
+  description = "DNS prefix for the AKS cluster"
+}
+
+variable "node_count" {
+  default     = 3
+  description = "Number of nodes in AKS cluster"
+}
+
+variable "vm_size" {
+  default     = "Standard_DS2_v2"
+  description = "VM size for AKS nodes"
 }
